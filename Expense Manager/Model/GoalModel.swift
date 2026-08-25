9331ee46cce7e42e71327b1d1e1602fffdc3c3ef
Shaheen107 <150262386+Shaheen107@users.noreply.github.com>
2026-08-25 23:56:40 +0500

    Format savedAmount declaration for consistency

--- Expense Manager/Model/GoalModel.swift
+++ Expense Manager/Model/GoalModel.swift
@@ -4,6 +4,6 @@ struct Goal: Codable {
     var name: String
     var amount: Double
     var date: Date
-    var savedAmount: Double = 0.0 // Default saved amount is 0
+    var savedAmount: Double = 0.0 // Default saved amount is 0     
     var category: String? // Optional category to link it with expenses
 }
